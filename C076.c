b3781b9fd9f93e519f7ff625bf87799bd70c5d06
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-10-21 03:46:21 +0900

    Create C076.c

---
+++ C076.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+    int X,Y,Z,N,i,sum,S[N];
+    scanf("%d %d %d \n%d",&X,&Y,&Z,&N);
+    
+    for(i=0;i<N;i++){
+        scanf("%d %d",&S[i],&T[i]);
+        if(9<=S[i] && S[i]<17){
+             if(9<=T[i] && T[i]<17){
+                sum+=(17-S[i])*X+(T[i]-17)*Y+()*Z;
+             }else if(17 <=T[i] && T[i]<22){
+                sum+=;
+             }else{
+                sum+=;
+            }
+        }else if(17 <=S[i] && S[i]<22){
+            
+        }else{
+            
+        }
+        
+    }
+    printf("%d",sum);
+    
+}

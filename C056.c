32b5fd2baadcaa26acabff8e34557ff1b3f50b6e
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-10-21 03:50:02 +0900

    Create C056.c

---
+++ C056.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int N,M;
+    scanf("%d %d",&N,&M);
+    int i,a[N],b[N];
+    for(i=0;i<N;i++){
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    
+    
+}

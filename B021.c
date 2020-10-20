2f0967afff1f7002d7f4117dbdfcdd4aba033b75
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-10-21 03:57:48 +0900

    Create B021.c

---
+++ B021.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+    int N;
+    scanf("%d",&N);
+    char a[N];
+    for(i=0;i<N;i++){
+        scanf("%s",&a[i]);
+    }
+    if(a[i][strlen(a[i])]){
+        
+    }else if(){
+        
+    }else if( && ){
+        
+    }
+}

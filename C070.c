676850c84cc9f984d030bb1b2d8e7e180eb45ca5
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-10-21 18:42:46 +0900

    Update C070.c

--- C070.c
+++ C070.c
@@ -2,30 +2,32 @@
 int main(void){
     // 自分の得意な言語で
     // Let's チャレンジ！！
-
-    int N,i,j;
+    int N,i;
     scanf("%d",&N);
-    int s[N];
-    unsigned char s;
-    
-    //数値を文字列変換して、型キャスト
+    char s[N][4];
     for(i=0;i<N;i++){
-        scanf("%d",&s[i]);
-        s = (char)s[i];
-    }
+        scanf("%s",&s[i][4]);
     
-    //文字列の要素を比較して、条件分岐
-    for(i=0;i<N;i++){
     if(s[i][1]==s[i][2]&& s[i][1]==s[i][3]&&s[i][1]==s[i][4]){
-        printf("Four Card");
-    }else if(s[i][1]==s[i][2]&& s[i][1]==s[i][3] || s[i][1]==s[i][2]&&s[i][1]==s[i][4] || s[i][1]==s[i][3]&&s[i][1]==s[i][4] ||s[i][2]==s[i][3]&&s[i][2]==s[i][4]){
-        printf("Three Card");
-    }else if(s[i][1]==s[i][2]&&s[i][3]==s[i][4]||s[i][1]==s[i][4]&&s[i][2]==s[i][3]||s[i][1]==s[i][3]&&s[i][2]==s[i][4]){
-        printf("Two Pair");
-    }else if(s[i][1]==s[i][2]||s[i][1]==s[i][3]||s[i][1]==s[i][4]||s[i][2]==s[i][3]||s[i][2]==s[i][4]||s[i][3]==s[i][4]){
-        printf("One Pair");
+        printf("Four Card\n");
+    }else if(s[i][1]==s[i][2] && s[i][1]==s[i][3]){
+       printf("Three Card\n");
+    }else if(s[i][1]==s[i][2] && s[i][1]==s[i][4]){
+        printf("Three Card\n");
+    }else if(s[i][1]==s[i][3] && s[i][1]==s[i][4]){
+        printf("Three Card\n");
+    }else if(s[i][2]==s[i][3] && s[i][2]==s[i][4]){
+        printf("Three Card\n");
+    }else if(s[i][1]==s[i][2] && s[i][3]==s[i][4]){
+        printf("Two Pair\n");
+    }else if(s[i][1]==s[i][4] && s[i][2]==s[i][3]){
+        printf("Two Pair\n");
+    }else if(s[i][1]==s[i][3] && s[i][2]==s[i][4]){
+        printf("Two Pair\n");
+    }else if(s[i][2]==s[i][3] || s[i][2]==s[i][4] || s[i][3]==s[i][4]||s[i][1]==s[i][4]|| s[i][1]==s[i][3]||s[i][1]==s[i][2]){
+        printf("One Pair\n");
     }else{
-        printf("No Pair");
+        printf("No Pair\n");
     }
     }
 }

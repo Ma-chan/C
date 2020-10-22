5cc0ebc48495f0926225c5cc58e3058b70a36b80
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-10-22 15:20:46 +0900

    Create B.c

---
+++ B.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+   int X,N,M,i,j,s;
+    scanf("%d,%d,%d\n",&X,&N,&M);
+   char P[X][M],C[N];
+   
+    for(i=0;i<N;i++){
+        scanf("%s\n",&C[i]);
+    }
+    for(j=0;j<M;j++){
+     for(s=0;s<X;s++){
+        scanf("%s\n",&P[i][j]);
+       
+     }
+ 
+}
+}
